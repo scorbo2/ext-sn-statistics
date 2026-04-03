269123642d1fa3ca660bb7c54140fcb54d2acb0a
Steve Corbett <steve@corbett.ca>
2026-04-03 17:36:05 -0600

    Issue #2 - add StatisticsUtil and tests

---
+++ src/main/java/ca/corbett/snotes/extensions/statistics/Phrase.java
@@ -0,0 +1,12 @@
+package ca.corbett.snotes.extensions.statistics;
+
+/**
+ * A simple record to represent a phrase and its occurrence count.
+ *
+ * @param phrase Any text phrase of two or more words. It can be empty, but it cannot be null.
+ * @param occurrenceCount The number of times this phrase occurs in the analyzed notes. A non-negative integer.
+ * @author <a href="https://github.com/scorbo2">scorbo2</a>
+ * @since Snotes 2.0
+ */
+public record Phrase(String phrase, int occurrenceCount) {
+}
